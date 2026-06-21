949e7d43a3f13b2d0d222c6541dd83448a37accc
Yuriy Medvedev <yuriy.medvedev.33@mail.ru>
2026-06-21 17:34:10 +0300

    created models.py

---
+++ database/models.py
@@ -0,0 +1,31 @@
+from sqlalchemy import ForeignKey, BigInteger, String, Boolean, DateTime, text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from datetime import date, datetime
+
+class Base(DeclarativeBase):
+    pass
+
+class Vacancy(Base):
+    __tablename__ = "vacancies"
+    id: Mapped[str] = mapped_column(String(50), primary_key=True)
+    title: Mapped[str] = mapped_column(String(255))
+    company: Mapped[str] = mapped_column(String(255))
+    url: Mapped[str] = mapped_column(String(512))
+
+    is_sent: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_applied: Mapped[bool] = mapped_column(Boolean, default=False)
+    parsed_at: Mapped[datetime] = mapped_column(
+        DateTime, 
+        server_default=text("TIMEZONE('utc', now())")
+    )
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    username: Mapped[str | None] = mapped_column(String(32))
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, 
+        server_default=text("TIMEZONE('utc', now())")
+    )
