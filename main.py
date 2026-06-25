020d118d9460ce307a6a342758ccef481dfa6ffe
Yuriy Medvedev <yuriy.medvedev.33@mail.ru>
2026-06-25 15:28:03 +0300

    modifying main.py added a timer trigger feature

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import asyncio
 from os import getenv
+from dotenv import load_dotenv
 import logging
 
 from aiogram import Bot, Dispatcher
 from bot.handlers import get_routers
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from bot.utils.scheduler_tasks import check_and_sent
 
 logging.basicConfig(
     level=logging.INFO,
@@ -13,20 +17,34 @@ logger = logging.getLogger("VacancyBot.Main")
 
 
 async def main():
-    dp = Dispatcher()
+    load_dotenv()
     token = getenv("BOT_TOKEN")
     if not token:
         raise ValueError("No token provided")
 
+    dp = Dispatcher()
     bot = Bot(token=token)
     dp.include_routers(*get_routers())
+
+    scheduler = AsyncIOScheduler(timezone="UTC")
+
+    scheduler.add_job(
+        check_and_sent,
+        trigger="interval",
+        seconds=1800,
+        kwargs={"bot": bot}
+    )
+
+    scheduler.start()
+    logger.info("Фоновый планировщик задач успешно запущен")
+
+    logger.info("Бот выходит в онлайн...")
 
     try:
         await dp.start_polling(bot)
     except Exception as e:
         logger.error(f"Произошла ошибка во время работы бота: {e}")
     finally:
-        # Закрываем сессию бота для чистого выхода
         await bot.session.close()
         logger.info("Бот полностью остановлен.")
 
